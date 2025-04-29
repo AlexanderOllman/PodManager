16bae599f9d7d046aa6671cb8002422db3c3ad11
AlexanderOllman <alexander.ollman@gmail.com>
2025-04-29 00:11:52 -0700

    updated version

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ echo "Pulling latest..."
 git pull
 
 echo "Building image..."
-docker build -t fheonix/pod-manager:0.0.1 .
+docker build -t fheonix/pod-manager:0.0.2 .
 
 echo "Pushing image..."
-docker push fheonix/pod-manager:0.0.1
+docker push fheonix/pod-manager:0.0.2
 
 echo "Done."
 
